e84c363143dc0d06abbdf5499e03fb93887e35fe
toyoda_k <toyoda@icepp.s.u-tokyo.ac.jp>
2021-01-10 12:32:21 +0900

    Single Event

--- lib/feature/home/home.dart
+++ lib/feature/home/home.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+/*
 /// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+class EventCards extends StatelessWidget {
+  EventCards({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +38,8 @@ class MyStatelessWidget extends StatelessWidget {
     );
   }
 }
+ */
 
-
-
-
-/*
-
-import 'package:flutter/material.dart';
-
-class HomeApp extends StatelessWidget {
-  static const String _title = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        body: EventCards(),
-      ),
-    );
-  }
-}
-
-/// Region for Event Cards
 class EventCards extends StatelessWidget {
   EventCards({Key key}) : super(key: key);
 
@@ -84,7 +64,7 @@ class EventCard extends StatelessWidget {
               title: Text('ハッカソン運営大会'),
               subtitle: Text('2021年1月16日土曜日 18:00JST'),
             ),
-            Image.network('https://yu1204eng.work/images/sample.jpg'),
+            Image.network('https://hakuhin.jp/graphic/title.png'),
             const ListTile(
               subtitle: Text('ハッカソンの運営は非常に難しいものであり，時間にルーズでなくてはなりません。なんでな'),
             ),
@@ -106,67 +86,3 @@ class EventCard extends StatelessWidget {
     );
   }
 }
-
-/// Flutter code sample for Card
-
-// This sample shows creation of a [Card] widget that shows album information
-// and two actions.
-
-void main() => runApp(MyApp());
-
-/// This is the main application widget.
-class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: MyStatelessWidget(),
-      ),
-    );
-  }
-}
-
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.album),
-              title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
- */
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: MyStatelessWidget(),
+        body: EventCards(),
       ),
     );
   }
