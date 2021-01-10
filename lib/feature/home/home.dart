a7750716e3523335da7c8639606d43ed7019f4bf
Keito <54165015+Tanesan@users.noreply.github.com>
2021-01-11 02:24:47 +0900

    いや〜なんか2時間かかってもうた

--- lib/feature/home/home.dart
+++ lib/feature/home/home.dart
@@ -5,7 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import "package:intl/intl.dart";
 
-class EventCards extends StatelessWidget {
+class EventCards extends StatefulWidget {
+  @override
+  _EventCardsState createState() => _EventCardsState();
+}
+
+class _EventCardsState extends State<EventCards> {
   String formatTimestamp(Timestamp timestamp) {
     initializeDateFormatting("ja_JP");
     DateTime dateTime = timestamp.toDate();
@@ -14,7 +19,7 @@ class EventCards extends StatelessWidget {
     return formatted;
   }
 
-  Future getURL(String documentID) async {
+  Future<String> getURL(String documentID) async {
     final String downloadURL = await firebase_storage.FirebaseStorage.instance
         .ref('event_images/' + documentID + '.png')
         .getDownloadURL();
@@ -30,7 +35,8 @@ class EventCards extends StatelessWidget {
         if (snapshot.hasError) {
           return Container(color: Colors.white);
         }
-
+        Image images_output;
+        Image _images_output;
         // Firebaseのinitialize完了したら表示したいWidget
         if (snapshot.connectionState == ConnectionState.done) {
           return StreamBuilder<QuerySnapshot>(
@@ -49,15 +55,18 @@ class EventCards extends StatelessWidget {
                   return new Text('Loading...');
                 default:
                   List<DocumentSnapshot> events = snapshot.data.docs;
+
                   return ListView.builder(
                     itemBuilder: (BuildContext context, int index) {
                       if (index == 0) return Container();
-                      final url =
-                          getURL(events[index - 1].reference.id).toString();
 
+                      getURL(events[index - 1].reference.id).then((content) {
+                        return Image.network(content);
+                      });
+                      //   print("これがりんくでございやす" + url);
                       return Center(
-                        child: Card(
-                          child: Column(
+                          child: Card(
+                        child: Column(
                             mainAxisSize: MainAxisSize.min,
                             children: <Widget>[
                               ListTile(
@@ -65,28 +74,32 @@ class EventCards extends StatelessWidget {
                                 subtitle: Text(formatTimestamp(
                                     events[index - 1]["event_date"])),
                               ),
-                              Image.network(url),
-                              //Image.network(getURL(events[index - 1].reference.id)),
-                              ListTile(
-                                subtitle:
-                                    Text(events[index - 1]["event_details"]),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  TextButton(
-                                    child: const Text('JOIN'),
-                                    onPressed: () {
-                                      /* ... */
-                                    },
-                                  ),
-                                  const SizedBox(width: 8)
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
+                              //     Image.network(url),
+                              FutureBuilder<String>(
+                                  future: getURL(events[index - 1].reference.id)
+                                      .then((content) {
+                                    images_output = Image.network(content);
+                                    return "A";
+                                  }),
+                                  builder: (context, snapshot) {
+                                    List<Widget> children;
+                                    if (snapshot.hasData) {
+                                      children = <Widget>[images_output];
+                                    } else {
+                                      children = <Widget>[Container()];
+                                    }
+                                    return Center(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: children,
+                                      ),
+                                    );
+                                  }),
+                            ]),
+                      ));
                     },
                     itemCount: events.length + 1,
                   );
