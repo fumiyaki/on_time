17c9fc279b61ce836ec7a9375655aa0d1d4f2461
ykt345 <yoppy6913@gmail.com>
2021-01-12 01:17:39 +0900

    fixing main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'features/event_setup/widgets/event_setup.dart';
 import 'features/event_setup/widgets/event_setup_test2.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
 
 /*void main() {
   runApp(MyApp());
