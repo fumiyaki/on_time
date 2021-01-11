6399c30691866fc2a1b4dd038ccedf2b9a117a8d
toyoda_k <toyoda@icepp.s.u-tokyo.ac.jp>
2021-01-11 17:04:45 +0900

    comments

--- lib/feature/home/home.dart
+++ lib/feature/home/home.dart
@@ -3,9 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:share/share.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import "package:intl/intl.dart" as intl;
-import 'package:flutter/gestures.dart';
 import "../schedule/schedule.dart";
 
 class EventCards extends StatefulWidget {
@@ -23,19 +20,22 @@ class _EventCardsState extends State<EventCards> {
 
   @override
   Widget build(BuildContext context) {
+    // Firebaseの初期化
     return FutureBuilder(
       future: Firebase.initializeApp(),
       builder: (context, snapshot) {
-        // エラー時に表示するWidget
+        // Firebaseの初期化エラー
         if (snapshot.hasError) {
-          return Container(color: Colors.white);
+          return Center(child: Text('エラーが発生しています'));
         }
 
-        // Firebaseのinitialize完了したら表示したいWidget
         if (snapshot.connectionState == ConnectionState.done) {
+          // Firestoreからイベントリストを取得
           return StreamBuilder<QuerySnapshot>(
             stream: FirebaseFirestore.instance
                 .collection('events')
+
+                // 過去（1日以上前）のイベントは表示しない
                 .where('event_date',
                     isGreaterThan: DateTime.now().add(Duration(days: 1) * -1))
 //                isGreaterThan: DateTime.now().add(Duration(days: 10)))
@@ -43,29 +43,42 @@ class _EventCardsState extends State<EventCards> {
                 .snapshots(),
             builder:
                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError)
+              // Firestoreからデータ取得する際のエラー
+              if (snapshot.hasError) {
                 return new Text('Error: ${snapshot.error}');
+              }
+
               switch (snapshot.connectionState) {
+
+                // Firestoreに問い合わせ中の表示
                 case ConnectionState.waiting:
                   return CircularProgressIndicator();
+
                 default:
                   List<DocumentSnapshot> events = snapshot.data.docs;
+
+                  // 表示すべきイベントがない場合
                   if (events.isEmpty) {
                     return Center(child: Text('登録されたイベントがありません'));
                   }
+
                   return ListView.builder(
                     itemBuilder: (BuildContext context, int index) {
                       if (index == 0) return Container();
+
                       DateTime eventDate =
                           events[index - 1]["event_date"].toDate();
                       final double numberSize = 20.0;
                       final double letterSize = 10.0;
+
+                      // 1イベント分のCard
                       return Card(
                         margin: const EdgeInsets.symmetric(
                             vertical: 10.0, horizontal: 30.0),
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
+                            // イベント名
                             Row(children: [
                               SpaceBox.width(30),
                               Expanded(
@@ -76,6 +89,8 @@ class _EventCardsState extends State<EventCards> {
                                               fontWeight: FontWeight.bold,
                                               fontSize: 25,
                                               color: Colors.grey[850])),
+
+                                      // 共有ボタン
                                       trailing: IconButton(
                                           icon: Icon(Icons.share),
                                           onPressed: () {
@@ -83,6 +98,8 @@ class _EventCardsState extends State<EventCards> {
                                                 "Link to open this particular event in OnTime App");
                                           }))),
                             ]),
+
+                            // 開催日時
                             Row(children: [
                               SpaceBox.width(50),
                               Text(
@@ -95,6 +112,8 @@ class _EventCardsState extends State<EventCards> {
                                   style: TextStyle(
                                       fontSize: letterSize,
                                       color: Colors.grey[800]),
+
+                                  // 異なる文字サイズを下揃えする
                                   strutStyle: StrutStyle(
                                     fontSize: numberSize,
                                   )),
@@ -142,36 +161,44 @@ class _EventCardsState extends State<EventCards> {
                                   )),
                             ]),
                             SpaceBox.height(10),
+
+                            // イベント画像
                             FutureBuilder(
                                 future: getURL(events[index - 1].reference.id),
                                 builder: (BuildContext context,
                                     AsyncSnapshot<String> snapshot) {
+                                  // 画像URL取得中の表示
                                   if (snapshot.connectionState !=
                                       ConnectionState.done) {
                                     return CircularProgressIndicator();
                                   }
-                                  if (snapshot.hasError) {
-                                    return Text(snapshot.error.toString());
-                                  }
+
                                   if (snapshot.hasData) {
                                     return ConstrainedBox(
                                         constraints:
                                             BoxConstraints(maxHeight: 150),
                                         child: Image.network(snapshot.data));
                                   } else {
+                                    // 画像取得エラー
                                     return Container();
                                   }
                                 }),
                             SpaceBox.height(10),
+
+                            // イベント詳細
                             Row(children: [
                               SpaceBox.width(30),
-                              Expanded(child: Text(events[index - 1]["event_details"],
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 3,
-                                  style: TextStyle(color: Colors.grey[700])),),
+                              Expanded(
+                                child: Text(events[index - 1]["event_details"],
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 3, // 3行以上の説明文は省略表示
+                                    style: TextStyle(color: Colors.grey[700])),
+                              ),
                               SpaceBox.width(30)
                             ]),
                             SpaceBox.height(5),
+
+                            // 詳細ボタン
                             Row(
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: <Widget>[
@@ -186,12 +213,15 @@ class _EventCardsState extends State<EventCards> {
                                               BorderRadius.circular(3)),
                                       onPressed: () {
                                         /*
-                                    Navigator.pushNamed(
-                                      context,
-                                      SchedulePage().routeName,
-                                      arguments: events[index - 1].reference.id
-                                    );
-                                    */
+                                        // スケジュール画面への遷移
+                                        Navigator.pushNamed(
+                                          context,
+                                          SchedulePage().routeName,
+
+                                        // eventsコレクションのドキュメントIDを渡す
+                                          arguments: events[index - 1].reference.id
+                                        );
+                                        */
                                       },
                                     )),
                                 SpaceBox.width(10)
@@ -208,13 +238,14 @@ class _EventCardsState extends State<EventCards> {
             },
           );
         }
-        // Firebaseのinitializeが完了するのを待つ間に表示するWidget
+        // Firebase初期化中の表示
         return CircularProgressIndicator();
       },
     );
   }
 }
 
+// マージン記述簡略化用Widget
 class SpaceBox extends SizedBox {
   SpaceBox({double width = 8, double height = 8})
       : super(width: width, height: height);
