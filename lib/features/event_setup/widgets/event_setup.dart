4a9c23210724e8f6b19ee57fd1da991122020cad
ykt345 <yoppy6913@gmail.com>
2021-01-13 00:08:40 +0900

    create feature ・textfields ・uploading data to the firestore

--- lib/features/event_setup/widgets/event_setup.dart
+++ lib/features/event_setup/widgets/event_setup.dart
@@ -1,5 +1,7 @@
+//formのデータを送る
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -11,69 +13,126 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AddEventWidget(),
+      home: MyCustomForm(),
     );
   }
 }
 
-class AddEventWidget extends StatelessWidget{
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState(
+    );
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+  final EventDate = TextEditingController();
+  final EventTitle = TextEditingController();
+  final EventDetail = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    EventDate.dispose();
+    EventTitle.dispose();
+    EventDetail.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Shortcuts(
-          shortcuts: <LogicalKeySet, Intent>{
-            // Pressing space in the field will now move to the next field.
-            LogicalKeySet(LogicalKeyboardKey.space): const NextFocusIntent(),
-          },
-          child: FocusTraversalGroup(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 128),
-              child: Form(
-                autovalidateMode: AutovalidateMode.always,
-                onChanged: () {
-                Form.of(primaryFocus.context).save();
-                },
-                child: Column(
-                  children: [
-                    ConstrainedBox(
-                      constraints: BoxConstraints.tight(const Size(200, 50)),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'イベントの日付/開始時間',
-                        ),
-                        onSaved: (String value) {
-                          //print('Value for field $index saved as "$value"');
-                        },
-                      ),
+    // Create a CollectionReference called users that references the firestore collection
+    CollectionReference addevents = FirebaseFirestore.instance.collection('events');
+
+    Future<void> addEvents() {
+      // Call the user's CollectionReference to add a new user
+      return addevents
+          .add({
+        'event_date': EventDate.text,
+        'event_title': EventTitle.text,
+        'event_detail': EventDetail.text
+      })
+          .then((value) => print("Event Added"))
+          .catchError((error) => print("Failed to add event: $error"));
+    }
+
+/*  @override
+  Widget build(BuildContext context) {
+  }*/
+
+    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OnTime'),
+      ),
+      body: Center(
+        child: FocusTraversalGroup(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 64),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: EventDate,
+                    decoration: const InputDecoration(
+                      labelText: 'イベントの日付/開始時間',
                     ),
-                    SizedBox(height: 16),
-                    ConstrainedBox(
-                      constraints: BoxConstraints.tight(const Size(200, 50)),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'イベントのタイトル',
-                        ),
-                        onSaved: (String value) {
-                          //print('Value for field $index saved as "$value"');
-                        },
-                      ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: EventTitle,
+                    decoration: const InputDecoration(
+                      labelText: 'イベントのタイトル',
                     ),
-                    SizedBox(height: 16),
-                    ConstrainedBox(
-                      constraints: BoxConstraints.tight(const Size(200, 50)),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'イベントの内容',
-                        ),
-                        onSaved: (String value) {
-                          //print('Value for field $index saved as "$value"');
-                        },
-                      ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: EventDetail,
+                    decoration: const InputDecoration(
+                      labelText: 'イベントの内容',
                     ),
-                    ]
-                ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+/*                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Scaffold.of(context)
+                              .showSnackBar(SnackBar(content: Text('Processing Data')));
+                        }
+                      },*/
+                      onPressed: addEvents,
+                      child: Text('Submit'),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'features/event_setup/widgets/event_setup.dart';
-import 'features/event_setup/widgets/event_setup_test2.dart';
+import 'features/event_setup/widgets/event_setup.dart';
 import 'package:firebase_core/firebase_core.dart' as firebase_core;
-
-/*void main() {
-  runApp(MyApp());
-}*/
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
