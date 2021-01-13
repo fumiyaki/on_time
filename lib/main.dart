7a27d3e0d4540678db5ed402f7f1a73f19fb1e5a
Keito <54165015+Tanesan@users.noreply.github.com>
2021-01-14 00:25:49 +0900

    Revert "Chat application"

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-import 'package:firebase_core/firebase_core.dart' as firebase_core;
 import 'package:flutter/material.dart';
 import 'feature/home/home.dart';
 
-import 'feature/chat/chat.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await firebase_core.Firebase.initializeApp();
+void main() {
   runApp(new SearchBarDemoApp());
 }
