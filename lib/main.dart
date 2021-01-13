bba75fdc423c1b205417c96a6cef731591d96160
toyoda_k <toyoda@icepp.s.u-tokyo.ac.jp>
2021-01-14 00:39:42 +0900

    changed to open chat app. temporary commit to merge develop again.

--- lib/main.dart
+++ lib/main.dart
@@ -8,5 +8,27 @@ import 'feature/chat/chat.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await firebase_core.Firebase.initializeApp();
-  runApp(new SearchBarDemoApp());
+  runApp(MyApp());
+}
+//void main() => runApp(SearchBarDemoApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'OnTime';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: _title,
+        home: MyHomePage(),
+//    home: SearchBarDemoHome(),
+        routes: <String, WidgetBuilder> {
+/*
+        AuthPage.routeName: (context) => AuthPage(),
+        SchedulePage.routeName: (context) => SchedulePage(),
+        ChatPage.routeName: (context) => MyHomePage()
+*/
+        }
+    );
+  }
 }
