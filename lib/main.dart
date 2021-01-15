b0f77be9dbad5baf4832e83a5fc449999782626d
toyoda_k <toyoda@icepp.s.u-tokyo.ac.jp>
2021-01-15 18:40:12 +0900

    implemented Dynamic Links

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart' as firebase_core;
 import 'package:flutter/material.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'features/home/home.dart';
-import 'features/auth/auth.dart';
 import 'features/schedule/schedule.dart';
+import 'features/auth/auth.dart';
+import 'features/event_setup/widgets/event_setup.dart';
 import 'features/chat/chat.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await firebase_core.Firebase.initializeApp();
+  dynamicLinksHandlerNonInstall();
   runApp(MyApp());
 }
 
@@ -24,11 +27,24 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder> {
         '/': (context) => Home(),
 /*
-        '/auth': (context) => AuthPage(),
         '/schedule': (context) => SchedulePage(),
+        '/auth': (context) => AuthPage(),
 */
+//        '/setup': (context) => EventSetup(),
+        '/setup': (context) => MyCustomForm(),
         '/chat': (context) => MyHomePage()
       }
     );
   }
 }
+
+
+/// Dynamic Link対応
+/// 未インストールの場合
+/// dynamicLinksHandlerNonInstall を main() 内で実行
+void dynamicLinksHandlerNonInstall() async {
+  final data = await FirebaseDynamicLinks.instance.getInitialLink();
+  final Uri deepLink = data?.link;
+  /// do something...
+}
+
