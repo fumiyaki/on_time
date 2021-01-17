7e146a0901fc824f7488c555150268f751cb23d5
ykt345 <yoppy6913@gmail.com>
2021-01-17 12:13:15 +0900

    feature => features

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_search_bar/flutter_search_bar.dart';
-import 'package:ontime/feature/edit/edit.dart';
+import 'package:ontime/features/edit/edit.dart';
 
 class SearchBarDemoApp extends StatelessWidget {
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ontime/feature/home/home.dart';
+import 'package:ontime/features/home/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 /*void main() {
